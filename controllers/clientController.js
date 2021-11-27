a537183ef287cb69e6a1fdfe58269b8bd43cd676
Shivaanee <89640504+Shivaanee@users.noreply.github.com>
2021-11-27 16:10:02 +0400

    Update clientController.js

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -24,7 +24,12 @@ const loginControl = (request, response) => {
                     //add to session
                     request.session.user = username;
                     request.session.num_client = client[0].num_client;
-                    request.session.admin = false;
+                    if (request.session.user=="Masson") {
+                        request.session.admin = true;
+                    } else {
+                        request.session.admin = false;
+                    }
+                    num_client_fromLogin = request.session.num_client;
                     response.render('afterLoginP', { user: client});
                     //response.render('contacts', { user: client});
                 }
@@ -39,7 +44,7 @@ const registerControl = (request, response) => {
     const { Client } = require('../models/entities');
 
     let username = request.body.username;
-    let password = request.body.passwsord;
+    let password = request.body.password;
     let society = request.body.society;
     let contact = request.body.contact;
     let addres = request.body.addres;
@@ -54,37 +59,67 @@ const registerControl = (request, response) => {
         console.log("User from register service :" + insertedID);
         if (exists) {
             console.log("Username taken!");
-            response.send(`registration failed. Username (${username}) already taken!`); //invite to register
+            //response.send(`registration failed. Username (${username}) already taken!`); //invite to register
+            let reply = "registration failed. Username (" + username + ") already taken!";
+            response.render("registerF", {user: reply});
         } else {
             client.num_client = insertedID;
             console.log(`Registration (${username}, ${insertedID}) successful!`);
-            response.send(`Successful registration ${client.contact} (ID.${client.num_client})!`);
+            //response.send(`Successful registration ${client.contact} (ID.${client.num_client})!`);
+            let reply = `Successful registration ` + client.contact + ` (ID.` + client.num_client + `)!`;
+            response.render("registerP", {user: reply});
         }
-        response.end();
+        //response.end();
     });
 };
 
 const getClients = (request, response) => {
     const clientServices = require('../services/clientServices');
-    clientServices.searchService(function(err, rows) {
-        response.json(rows);
-        response.end();
-    });
+    if (request.session.admin==true) {
+        clientServices.searchService(function(err, rows) {
+            // response.json(rows);
+            // response.end();
+            console.log(JSON.stringify(rows));
+            response.render("clients", {clients: rows});
+        }); 
+    } else {
+        let reply = "Not An Admin";
+        console.log(reply);
+        response.render("clientsF", {clients: reply});
+    }
 };
 
 const getClientByNumclient = (request, response) => {
     const clientServices = require('../services/clientServices');
-    let num_client = request.params.num_client;
-    clientServices.searchNumclientService(num_client, function(err, rows) {
-        // response.json(rows);
-        // response.end();
-        response.render('contacts', {clients: rows});
-    });
+    let num_client = request.session.num_client;
+    if (num_client!=null) {
+        clientServices.searchNumclientService(num_client, function(err, rows) {
+            // response.json(rows);
+            // response.end();
+            response.render('contacts', {clients: rows});
+        });
+    } else {
+        console.log("No logged in user");
+        response.send("Please login first");
+        response.end();
+    }
 };
+
+const logout = (request, response) => {
+    if (!request.session) {
+        let reply = "Please log in first";
+        response.render("logout", {user: reply});
+    } else {
+        let reply = "You have been logged out, " + request.session.user;
+        request.session.destroy();
+        response.render("logout", {user: reply});
+    }
+}
 
 module.exports = {
     loginControl,
     registerControl,
     getClients,
-    getClientByNumclient
+    getClientByNumclient,
+    logout
 };
